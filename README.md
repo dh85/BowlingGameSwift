4167b935e213e2329164fb4d0d5c4d83d8b6b79b
David Hughes <39618866+dh85@users.noreply.github.com>
2023-07-21 09:46:08 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BowlingGameSwift
+
+A description of this package.
---
+++ Sources/BowlingGameSwift/BowlingGameSwift.swift
@@ -0,0 +1,6 @@
+public struct BowlingGameSwift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
